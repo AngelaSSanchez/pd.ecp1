7031b017a30d455d9dd6fbde3b9309b990d4c661
AngelaS <AngelaS@192.168.1.138>
2015-10-17 21:17:09 +0200

    composite.expression Se implementa la clase Dividir

--- src/main/java/es/upm/miw/pd/composite/expression/Dividir.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Dividir.java
@@ -2,4 +2,19 @@ package es.upm.miw.pd.composite.expression;
 
 public class Dividir extends Operacion{
 
+	private String operador;
+	
+	public Dividir(Expresion exp1, Expresion exp2) {
+		super(exp1, exp2);
+		this.operador = "/";
+	}
+	
+	public String getOperador(){
+		return this.operador;
+	}
+
+	@Override
+	public int operar() {
+		return this.exp1.operar() / this.exp2.operar();
+	}
 }

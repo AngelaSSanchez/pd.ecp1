7f237a4d9a526c9f094e5bbea3994adff4857938
AngelaS <AngelaS@192.168.1.138>
2015-10-17 21:21:02 +0200

    composite.expression Se implementa la clase Operación y se pasan las
    pruebas unitarias de ExpressionTest

--- src/main/java/es/upm/miw/pd/composite/expression/Operacion.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Operacion.java
@@ -1,5 +1,19 @@
 package es.upm.miw.pd.composite.expression;
 
 public abstract class Operacion extends Expresion{
-
+	
+	protected Expresion exp1, exp2;
+	
+	public Operacion(Expresion exp1, Expresion exp2){
+		this.exp1 = exp1;
+		this.exp2 = exp2;
+	}
+	
+	public String getOperador(){
+		return getOperador();
+	}
+	
+	public String toString(){
+		return "("+this.exp1.toString()+ getOperador() +this.exp2.toString()+")";
+	}
 }

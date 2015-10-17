71ac7cb003ed66efa93ffaf59091abb8e1dbd73f
AngelaS <AngelaS@192.168.1.138>
2015-10-17 21:16:54 +0200

    composite.expression Se implementa la clase Multiplicar

--- src/main/java/es/upm/miw/pd/composite/expression/Multiplicar.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Multiplicar.java
@@ -2,4 +2,19 @@ package es.upm.miw.pd.composite.expression;
 
 public class Multiplicar extends Operacion{
 
+	private String operador;
+	
+	public Multiplicar(Expresion exp1, Expresion exp2) {
+		super(exp1, exp2);
+		this.operador = "*";
+	}
+	
+	public String getOperador(){
+		return this.operador;
+	}
+
+	@Override
+	public int operar() {
+		return this.exp1.operar() * this.exp2.operar();
+	}
 }

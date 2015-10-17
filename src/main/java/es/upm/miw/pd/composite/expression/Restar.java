85b5deef580fb3e6dbc804254be3c755d075327d
AngelaS <AngelaS@192.168.1.138>
2015-10-17 21:16:28 +0200

    composite.expression Se implementa la clase Restar

--- src/main/java/es/upm/miw/pd/composite/expression/Restar.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Restar.java
@@ -1,5 +1,21 @@
 package es.upm.miw.pd.composite.expression;
 
 public class Restar extends Operacion{
+	
+	private String operador;
+	
+	public Restar(Expresion exp1, Expresion exp2) {
+		super(exp1, exp2);
+		this.operador = "-";
+	}
+	
+	public String getOperador(){
+		return this.operador;
+	}
+
+	@Override
+	public int operar() {
+		return this.exp1.operar() - this.exp2.operar();
+	}
 
 }

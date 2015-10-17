ca2d3e47526011999e2ad41d76b9925e8052d652
AngelaS <AngelaS@192.168.1.138>
2015-10-17 20:09:02 +0200

    composite.expression se implementa la clase 'hoja' Numero

--- src/main/java/es/upm/miw/pd/composite/expression/Numero.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Numero.java
@@ -2,4 +2,34 @@ package es.upm.miw.pd.composite.expression;
 
 public class Numero extends Expresion{
 
+	private int numero;
+
+	public Numero(int numero){
+		this.numero = numero;
+	}
+	
+	@Override
+	public void add(Expresion exp) {
+		throw new UnsupportedOperationException("Operación no permitida");
+	}
+
+	@Override
+	public void remove(Expresion exp) {
+	}
+
+	@Override
+	public boolean isComposite() {
+		return false;
+	}
+
+	@Override
+	public int operate() {
+		return this.numero;
+	}
+
+	@Override
+	public String toString() {
+		return Integer.toString(this.numero);
+	}
+	
 }

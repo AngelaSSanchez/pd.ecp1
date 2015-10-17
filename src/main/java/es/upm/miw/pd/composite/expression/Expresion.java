ed8b8ad40f614aa5c7a5d8321f52468afd0a0832
AngelaS <AngelaS@192.168.1.138>
2015-10-17 20:02:44 +0200

    composite.expression se definen los métodos abstractos de la clase
    Expresión que será el 'component'

--- src/main/java/es/upm/miw/pd/composite/expression/Expresion.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Expresion.java
@@ -2,4 +2,13 @@ package es.upm.miw.pd.composite.expression;
 
 public abstract class Expresion {
 
+	public abstract void add(Expresion exp);
+	
+	public abstract void remove(Expresion exp);
+	
+	public abstract boolean isComposite();
+	
+	public abstract int operate();
+	
+	public abstract String toString();
 }

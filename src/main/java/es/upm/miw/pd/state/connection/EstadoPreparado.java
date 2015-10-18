43058f81d62d64bbd2116ca343bbd3918648eee3
AngelaS <AngelaS@192.168.1.138>
2015-10-18 13:30:53 +0200

    state.conexion Se implementa la clase EstadoPreparado

--- src/main/java/es/upm/miw/pd/state/connection/EstadoPreparado.java
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoPreparado.java
@@ -1,5 +1,32 @@
 package es.upm.miw.pd.state.connection;
 
 public class EstadoPreparado extends EstadoConexion{
+	
+	private Link link;
 
+	public Estado getEstado(){
+		return Estado.PREPARADO;
+	}
+	
+	public void cerrar(Conexion conexion){
+		conexion.setEstado(new EstadoCerrado());
+	}
+	
+	public void abrir(Conexion conexion){
+		
+	}
+	
+	public void iniciar(Conexion conexion){
+		
+	}
+	
+	public void enviar(Conexion conexion, String msg){
+		this.link.enviar(msg);
+		conexion.setEstado(new EstadoEsperando());
+	}
+	
+	public void parar(Conexion conexion){
+		conexion.setEstado(new EstadoParado());
+	}
+	
 }

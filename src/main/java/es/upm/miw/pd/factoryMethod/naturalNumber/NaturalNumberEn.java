5e24bd4261b793a9fe035128125899ba0ebce341
AngelaS <AngelaS@192.168.1.138>
2015-10-18 15:52:34 +0200

    factoryMethod.naturalNumber Se implementan las clases NaturalNumberFr y
    NaturalNumberEn y se modifica la clase NaturalNumberEs para adaptarla al
    patrón

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEn.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEn.java
@@ -2,4 +2,12 @@ package es.upm.miw.pd.factoryMethod.naturalNumber;
 
 public class NaturalNumberEn extends NaturalNumber{
 
+	private String[] textValue;
+	
+	public NaturalNumberEn(int value) {
+		super(value);
+		textValue = new String[] {"zero", "one", "two", "three", "four", "five"};
+		setTextValues(textValue);
+	}
+
 }
--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberFr.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberFr.java
@@ -1,5 +1,13 @@
 package es.upm.miw.pd.factoryMethod.naturalNumber;
 
 public class NaturalNumberFr extends NaturalNumber{
+	
+	private String[] textValue;
+
+	public NaturalNumberFr(int value) {
+		super(value);
+		textValue = new String[] {"zéro", "un", "deux", "trois", "quatre", "cinq"};
+		setTextValues(textValue);
+	}
 
 }

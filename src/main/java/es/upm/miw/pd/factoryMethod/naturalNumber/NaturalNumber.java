4822e390eee440b5601b661ed12be0eab4b102ae
AngelaS <AngelaS@192.168.1.138>
2015-10-18 15:45:45 +0200

    factoryMethod.NaturalNumber Se corrige la clase NaturalNumber para
    implementar todos los métodos comunes de los diferentes idiomas

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumber.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumber.java
@@ -3,6 +3,8 @@ package es.upm.miw.pd.factoryMethod.naturalNumber;
 public abstract class NaturalNumber {
 	
 	private int value;
+	
+	private String[] textValue;
 
 	public NaturalNumber(int value){
 		this.setValue(value);
@@ -19,5 +21,21 @@ public abstract class NaturalNumber {
     
     public void add(int value) {
         this.setValue(this.value + value);
+    }
+   
+    public String[] getTextValues(){
+    	return this.getTextValues();
+    }
+    
+    public void setTextValues(){
+    	this.textValue = this.getTextValues();
+    }
+        
+    public String getTextValue() {
+        if (this.value < this.textValue.length) {
+            return this.textValue[this.value];
+        } else {
+            return "???";
+        }
     }
 }

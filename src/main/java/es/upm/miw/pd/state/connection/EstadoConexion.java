3658776c6aedcbfe73b4b6f5b7dc5d27bf158895
AngelaS <AngelaS@192.168.1.138>
2015-10-18 13:09:51 +0200

    PATRON STATE Se crea la clase EstadoConexión que define las acciones
    de los estados de la conexion

---
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoConexion.java
@@ -0,0 +1,33 @@
+package es.upm.miw.pd.state.connection;
+
+public abstract class EstadoConexion {
+	
+	public Estado getEstado(){
+		return getEstado();
+	}
+	
+	public void abrir(Conexion conexion){
+		
+	}
+	
+	public void cerrar(Conexion conexion){
+		
+	}
+	
+	public void parar(Conexion conexion){
+		
+	}
+	
+	public void iniciar(Conexion conexion){
+		
+	}
+	
+	public void enviar(Conexion conexion){
+		
+	}
+	
+	public void recibir(Conexion conexion){
+		
+	}
+	
+}

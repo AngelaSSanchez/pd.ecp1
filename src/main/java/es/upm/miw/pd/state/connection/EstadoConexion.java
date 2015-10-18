279fc344a5608531809a90110ac7152c5200cbbc
AngelaS <AngelaS@192.168.1.138>
2015-10-18 13:39:07 +0200

    state.conexion Se añaden las excepciones no permitidas a las acciones de
    Estado Conexion y se añaden parámetros a enviar y recibir

--- src/main/java/es/upm/miw/pd/state/connection/EstadoConexion.java
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoConexion.java
@@ -7,27 +7,27 @@ public abstract class EstadoConexion {
 	}
 	
 	public void abrir(Conexion conexion){
-		
+		throw new UnsupportedOperationException("Acción no permitida");
 	}
 	
 	public void cerrar(Conexion conexion){
-		
+		throw new UnsupportedOperationException("Acción no permitida");
 	}
 	
 	public void parar(Conexion conexion){
-		
+		throw new UnsupportedOperationException("Acción no permitida");
 	}
 	
 	public void iniciar(Conexion conexion){
-		
+		throw new UnsupportedOperationException("Acción no permitida");
 	}
 	
-	public void enviar(Conexion conexion){
-		
+	public void enviar(Conexion conexion, String msg){
+		throw new UnsupportedOperationException("Acción no permitida");
 	}
 	
-	public void recibir(Conexion conexion){
-		
+	public void recibir(Conexion conexion, int respuesta){
+		throw new UnsupportedOperationException("Acción no permitida");
 	}
 	
 }

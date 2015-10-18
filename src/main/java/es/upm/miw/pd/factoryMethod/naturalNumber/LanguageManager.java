2bdd062f95bcb8e12dad8786883278179d7e4346
AngelaS <AngelaS@192.168.1.138>
2015-10-18 16:17:44 +0200

    factoryMethod.naturalNumber Se implementa la clase LanguageManager que
    hace la función de Factoria

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/LanguageManager.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/LanguageManager.java
@@ -2,4 +2,18 @@ package es.upm.miw.pd.factoryMethod.naturalNumber;
 
 public class LanguageManager {
 
+	private NaturalNumber naturalnumber;
+	private NaturalNumberCreator naturalnumbercreator;	
+	
+	public void createNaturalNumber(int value){
+		this.naturalnumber = this.naturalnumbercreator.createNaturalNumber(value);
+	}
+	
+	public void setNaturalNumber(NaturalNumberCreator naturalnumbercreator){
+		this.naturalnumbercreator = naturalnumbercreator;
+	}
+	
+	public NaturalNumber getNaturalNumber(){
+		return this.naturalnumber;
+	}
 }

2064569094ec9b8846ea86733013204bab611d35
AngelaS <AngelaS@192.168.1.138>
2015-10-18 13:20:22 +0200

    state.conexion Se crean las clases de los estados de la conexión

---
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoCerrado.java
@@ -0,0 +1,18 @@
+package es.upm.miw.pd.state.connection;
+
+public class EstadoCerrado extends EstadoConexion{
+	
+	private Estado estado;
+
+	public EstadoCerrado(){
+		this.estado = Estado.CERRADO;
+	}
+	
+	public void cerrar(Conexion conexion){
+		
+	}
+	
+	public void abrir(Conexion conexion){
+		conexion.setEstado(new EstadoPreparado());
+	}
+}

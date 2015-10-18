db348cfa8349b53d6297857b33dbb0527f52e00a
AngelaS <AngelaS@192.168.1.138>
2015-10-18 13:33:21 +0200

    state.conexion Se implementa la clase EstadoEsperando

--- src/main/java/es/upm/miw/pd/state/connection/EstadoEsperando.java
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoEsperando.java
@@ -2,4 +2,15 @@ package es.upm.miw.pd.state.connection;
 
 public class EstadoEsperando extends EstadoConexion{
 
+	public Estado getEstado(){
+		return Estado.ESPERANDO;
+	}
+	
+	public void recibir(Conexion conexion, int respuesta){
+        if (respuesta == 0) {
+            conexion.setEstado(new EstadoPreparado()); 
+        } else {
+        	conexion.setEstado(new EstadoCerrado()); 
+        }
+	}
 }

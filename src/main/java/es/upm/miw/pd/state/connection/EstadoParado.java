e84749f2365cb5876c849dd2060c8d756485615b
AngelaS <AngelaS@192.168.1.138>
2015-10-18 13:38:08 +0200

    state.conexion Se implementa la clase EstadoParado

--- src/main/java/es/upm/miw/pd/state/connection/EstadoParado.java
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoParado.java
@@ -2,4 +2,15 @@ package es.upm.miw.pd.state.connection;
 
 public class EstadoParado extends EstadoConexion{
 
+	public Estado getEstado(){
+		return Estado.PARADO;
+	}
+	
+	public void parar(Conexion conexion){
+		
+	}
+	
+	public void iniciar(Conexion conexion){
+		conexion.setEstado(new EstadoPreparado());
+	}
 }

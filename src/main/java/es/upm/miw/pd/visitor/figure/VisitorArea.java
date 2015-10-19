741aa6bf0cfe7b41c0c6dc7e250fd96f1c38373b
AngelaS <AngelaS@192.168.1.138>
2015-10-19 20:21:30 +0200

    visitor.figure Se crea la clase VisitorArea para calcular las áreas de
    los elementos

---
+++ src/main/java/es/upm/miw/pd/visitor/figure/VisitorArea.java
@@ -0,0 +1,23 @@
+package es.upm.miw.pd.visitor.figure;
+
+public class VisitorArea extends Visitor{
+
+	@Override
+	public void visitCircle(Figure f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visitSquare(Figure f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visitTriangle(Figure f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

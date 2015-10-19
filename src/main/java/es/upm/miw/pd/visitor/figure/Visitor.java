b22848e76c36468de393e8eb4530e7974afcf26e
AngelaS <AngelaS@192.168.1.138>
2015-10-19 20:45:40 +0200

    visitor.figure Se corrige el parámetro de la llamada a los visitor

--- src/main/java/es/upm/miw/pd/visitor/figure/Visitor.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/Visitor.java
@@ -2,10 +2,10 @@ package es.upm.miw.pd.visitor.figure;
 
 public abstract class Visitor {
 
-	public abstract void visitCircle(Figure f);
+	public abstract void visitCircle(Circle circle);
 	
-	public abstract void visitSquare(Figure f);
+	public abstract void visitSquare(Square square);
 	
-	public abstract void visitTriangle(Figure f);
+	public abstract void visitTriangle(Triangle triangle);
 	
 }

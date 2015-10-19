003950b4673e1ee5dc74f19a5dd11019c9a00d43
AngelaS <AngelaS@192.168.1.138>
2015-10-19 20:50:01 +0200

    visitor.figure Se calculan los números de lados de los elementos

--- src/main/java/es/upm/miw/pd/visitor/figure/VisitorNumberofSides.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/VisitorNumberofSides.java
@@ -2,22 +2,25 @@ package es.upm.miw.pd.visitor.figure;
 
 public class VisitorNumberofSides extends Visitor{
 
+	private double numofsides = 0;
+	
 	@Override
-	public void visitCircle(Figure f) {
-		// TODO Auto-generated method stub
-		
+	public void visitCircle(Circle circle) {
+		this.numofsides = Double.POSITIVE_INFINITY;
 	}
 
 	@Override
-	public void visitSquare(Figure f) {
-		// TODO Auto-generated method stub
-		
+	public void visitSquare(Square square) {
+		this.numofsides = 4;
 	}
 
 	@Override
-	public void visitTriangle(Figure f) {
-		// TODO Auto-generated method stub
-		
+	public void visitTriangle(Triangle triangle) {
+		this.numofsides = 3;
+	}
+
+	public double getNumofSides(){
+		return this.numofsides;
 	}
 
 }

7b61223bcd65bb45acba08ab9e5547868f10239b
AngelaS <AngelaS@192.168.1.138>
2015-10-19 20:47:34 +0200

    visitor.figure Se calculan las áreas de los elementos

--- src/main/java/es/upm/miw/pd/visitor/figure/VisitorArea.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/VisitorArea.java
@@ -2,22 +2,25 @@ package es.upm.miw.pd.visitor.figure;
 
 public class VisitorArea extends Visitor{
 
+	private double area = 0.0;
+	
 	@Override
-	public void visitCircle(Figure f) {
-		// TODO Auto-generated method stub
-		
+	public void visitCircle(Circle circle) {
+		this.area = Math.PI * circle.getRadius() * circle.getRadius();
 	}
 
 	@Override
-	public void visitSquare(Figure f) {
-		// TODO Auto-generated method stub
-		
+	public void visitSquare(Square square) {
+		this.area = square.getSide() * square.getSide();
 	}
 
 	@Override
-	public void visitTriangle(Figure f) {
-		// TODO Auto-generated method stub
-		
+	public void visitTriangle(Triangle triangle) {
+		this.area = triangle.getBase() * triangle.getHeight() * 0.5;
+	}
+	
+	public double getArea(){
+		return this.area;
 	}
 
 }

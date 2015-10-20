6b61dbe4010b457470e9bbe982d4b85d987a4e02
AngelaS <AngelaS@192.168.1.138>
2015-10-20 21:11:42 +0200

    Text Se implementa la clase Carácter

--- src/main/java/es/upm/miw/pd/text/Caracter.java
+++ src/main/java/es/upm/miw/pd/text/Caracter.java
@@ -2,4 +2,34 @@ package es.upm.miw.pd.text;
 
 public class Caracter extends Componente{
 
+	private char c;
+	
+	public Caracter(Character c){
+		this.c = c;
+	}
+	
+	public char getCaracter(){
+		return this.c;
+	}
+
+	@Override
+	public void add(Componente h) {
+	}
+
+	@Override
+	public String dibujar(boolean b) {
+		if (b){
+			return this.toString().toUpperCase();					
+		} 
+		return this.toString();
+	}
+
+	public String toString() {
+		return Character.toString(c);
+	}
+
+	@Override
+	public boolean isComposite() {
+		return false;
+	}
 }

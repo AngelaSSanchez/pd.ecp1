520dab147970f236511d7ddb4c4e51c6fa88f432
AngelaS <AngelaS@192.168.1.138>
2015-10-22 22:05:29 +0200

    calculator.memento Se implementa la clase Memento que guarda el valor

--- src/main/java/es/upm/miw/pd/command/calculator/MementoCalculator.java
+++ src/main/java/es/upm/miw/pd/command/calculator/MementoCalculator.java
@@ -2,5 +2,13 @@ package es.upm.miw.pd.command.calculator;
 
 public class MementoCalculator {
 
+	int value;
 	
+	public MementoCalculator(int value){
+		this.value = value;
+	}
+	
+	public int getTotal(){
+		return this.value;
+	}
 }

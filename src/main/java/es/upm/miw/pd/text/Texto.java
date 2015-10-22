bb05fc5c9984c266d82a0994e25be5fe5dc09d03
AngelaS <AngelaS@192.168.1.138>
2015-10-22 19:23:41 +0200

    text Se implementa la clase Texto que será parte del composite y se
    pasan las pruebas de la clase TextoTest

--- src/main/java/es/upm/miw/pd/text/Texto.java
+++ src/main/java/es/upm/miw/pd/text/Texto.java
@@ -1,5 +1,40 @@
 package es.upm.miw.pd.text;
 
-public class Texto extends Escrito{
+public class Texto extends Object{
+	
+	private String texto;
+	
+	public Texto(){
+		super();
+	}
+	
+	@Override
+	public void add(Componente h) throws UnsupportedOperationException{
+		if (h.isComposite()){
+			this.object.add(h);
+		} else {
+			throw new UnsupportedOperationException("Acción no permitida");
+		}
+	}
+	
+	public void remove(Componente h){
+		if (h.isComposite()){
+			this.object.remove(h);
+		}
+	}
+
+	@Override
+	public String dibujar(boolean b) {
+		this.texto ="";
+		for (Componente componente : object){
+			this.texto += componente.dibujar(b);
+		}
+		return this.texto + "---o---\n";
+	}
+
+	@Override
+	public boolean isComposite() {
+		return true;
+	}
 
 }

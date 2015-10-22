6f0be4346bfe5f9a56d59a608fcd892b5033a99d
AngelaS <AngelaS@192.168.1.138>
2015-10-22 22:09:34 +0200

    calculator.memento Se implementan las acciones Salvar y Deshacer de la
    calculadora

--- src/main/java/es/upm/miw/pd/command/calculator/SaveCommand.java
+++ src/main/java/es/upm/miw/pd/command/calculator/SaveCommand.java
@@ -1,5 +1,19 @@
 package es.upm.miw.pd.command.calculator;
 
-public class SaveCommand {
+public class SaveCommand extends ComandoAbstracto{
+
+	public SaveCommand(Calculator calculator) {
+		super(calculator);
+	}
+
+	@Override
+	public String name() {
+		return "Salvar";
+	}
+	
+	@Override
+	public void execute(){
+		this.getCalculator().save();
+	}	
 
 }
--- src/main/java/es/upm/miw/pd/command/calculator/UndoCommand.java
+++ src/main/java/es/upm/miw/pd/command/calculator/UndoCommand.java
@@ -1,5 +1,19 @@
 package es.upm.miw.pd.command.calculator;
 
-public class UndoCommand {
+public class UndoCommand extends ComandoAbstracto{
+
+	public UndoCommand(Calculator calculator) {
+		super(calculator);
+	}
+
+	@Override
+	public String name() {
+		return "Deshacer";
+	}
+	
+	@Override
+	public void execute(){
+		this.getCalculator().undo();
+	}	
 
 }

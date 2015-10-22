959b45d959df6c67727ea168ae2f11b8902b6f55
AngelaS <AngelaS@192.168.1.138>
2015-10-22 19:13:30 +0200

    text Se implementa la clase FactoriaCaracter con el patrón SINGLETON y
    para implementar el patron FLYWEIGHT

--- src/main/java/es/upm/miw/pd/text/FactoriaCaracter.java
+++ src/main/java/es/upm/miw/pd/text/FactoriaCaracter.java
@@ -1,10 +1,29 @@
 package es.upm.miw.pd.text;
 
-public class FactoriaCaracter {
+import java.util.HashMap;
+import java.util.Map;
 
-	public static Object getFactoria() {
-		// TODO Auto-generated method stub
-		return null;
+public class FactoriaCaracter {
+    private Map<Character, Caracter> map ;
+    
+    private static FactoriaCaracter factoriacaracter = new FactoriaCaracter();
+    
+    private FactoriaCaracter(){
+    	map = new HashMap<Character, Caracter>();
+    }
+
+    public Caracter get(char key) {
+        if (map.containsKey(key)) {
+            return map.get(key);
+        } else {
+            Caracter caracter = new Caracter(key);
+            map.put(key, caracter);
+            return caracter;
+        }
+    }
+
+	public static FactoriaCaracter getFactoria() {
+		return factoriacaracter;
 	}
 
 }

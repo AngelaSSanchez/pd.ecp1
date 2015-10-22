00bcd634327e1ca03f956edee1e211a368c35fed
AngelaS <AngelaS@192.168.1.138>
2015-10-22 19:23:01 +0200

    text Se implementa la clase Parrafo que será parte del composite

--- src/main/java/es/upm/miw/pd/text/Parrafo.java
+++ src/main/java/es/upm/miw/pd/text/Parrafo.java
@@ -1,5 +1,41 @@
 package es.upm.miw.pd.text;
 
-public class Parrafo extends Escrito{
+public class Parrafo extends Object{
+	
+	private String parrafo;
+
+	public Parrafo(){
+		super();
+	}
+	
+	@Override
+	public void add(Componente h) throws UnsupportedOperationException{
+		if (!h.isComposite()) {
+			this.object.add(h);
+		} 
+		else {
+			throw new UnsupportedOperationException("Acción no permitida");
+		}
+	}
+	
+	public void remove(Componente h){
+		if (!h.isComposite()){
+			this.object.remove(h);
+		}
+	}
+
+	@Override
+	public String dibujar(boolean b) {
+		this.parrafo = "";
+		for (Componente componente : object){
+			this.parrafo += componente.dibujar(b);
+		}
+		return this.parrafo+"\n";
+	}
+
+	@Override
+	public boolean isComposite() {
+		return true;
+	}
 
 }
